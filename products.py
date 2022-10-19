2735dadaee997b7266d955ca7c58740dfdc57975
Emerson Villalta <reaperfocs@gmail.com>
2022-10-19 12:22:11 -0500

    initial commit

---
+++ products.py
@@ -0,0 +1,7 @@
+products = [
+    { "name": "laptop", "price": 4500, "quantity": 8},
+    { "name": "SSD", "price": 500, "quantity": 40},
+    { "name": "mouse", "price": 40, "quantity": 35},
+    { "name": "Usb", "price": 100, "quantity": 15},
+    { "name": "laptopmark", "price": 4000, "quantity": 12}
+]
---
+++ readme.md
@@ -0,0 +1,1 @@
+<h1>CRUD WITH FLASK REST </h1>
